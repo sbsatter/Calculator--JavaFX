1a21ba1e1d43e0f11e6d48e571e83c551d58e9c9
Shahed Bin Satter <sbsatter@gmail.com>
2017-03-06 18:07:53 +0600

    Simple calculator

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,9 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -35,20 +37,82 @@ public class Main extends Application {
 	    root.add(btnAdd, 0, 0);
 	    root.add(btnSub, 1, 0);
 	    root.add(btnDiv, 0, 1);
-	    root.add(btnMult, 1, 0);
-	    root.add(btnClear, 0, 2);
-//	    root.add(btnClear, 0, 2);
-	    root.add(txtNum1, 0, 3);
-	    root.add(txtNum2, 1, 3);
+	    root.add(btnMult, 1, 1);
+	    root.add(txtNum1, 0, 2);
+	    root.add(txtNum2, 1, 2);
+	    root.add(ansLabel, 0, 3, 2, 1);
+	    root.add(btnClear, 0, 4, 2, 1);
+	    
+	    setWidths();
+	    attachCode();
+	
+	    Scene scene = new Scene(root, 300, 250);
+	    primaryStage.setScene(scene);
+	    primaryStage.setTitle("Calculator");
+	    primaryStage.show();
 //	    root.add(btnAdd, 2, 0)0;
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 //        primaryStage.setTitle("Hello World");
 //        primaryStage.setScene(new Scene(root, 300, 275));
 //        primaryStage.show();
     }
-
-
-    public static void main(String[] args) {
+	
+	private void attachCode() {
+		btnAdd.setOnAction(e -> btnCode(e));
+		btnSub.setOnAction(e -> btnCode(e));
+		btnDiv.setOnAction(e -> btnCode(e));
+		btnMult.setOnAction(e -> btnCode(e));
+//		btnEquals.setOnAction(e -> btnCode(e));
+		txtNum1.setOnAction(e -> btnCode(e));
+		txtNum2.setOnAction(e -> btnCode(e));
+		btnClear.setOnAction(e -> btnCode(e));
+	}
+	
+	private void btnCode(ActionEvent e) {
+		int num1, num2, answer;
+		char symbol;
+		
+		if (e.getSource() == btnClear) {
+			txtNum1.setText("");
+			txtNum2.setText("");
+			ansLabel.setText("?");
+			txtNum1.requestFocus();
+			return;
+		}
+		num1 = Integer.parseInt(txtNum1.getText());
+		num2 = Integer.parseInt(txtNum2.getText());
+		
+		if (e.getSource() == btnAdd) {
+			symbol = '+';
+			answer = num1 + num2;
+		} else if (e.getSource() == btnSub) {
+			symbol = '-';
+			answer = num1 - num2;
+		} else if (e.getSource() == btnDiv) {
+			symbol = '/';
+			answer = num1 / num2;
+		} else {
+			symbol = '*';
+			answer = num1 * num2;
+		}
+		
+		ansLabel.setText(num1 + " " + symbol + " " +num2 + " = " + answer);
+	}
+	
+	private void setWidths() {
+		btnAdd.setPrefWidth(70);
+		btnSub.setPrefWidth(70);
+		btnDiv.setPrefWidth(70);
+		btnMult.setPrefWidth(70);
+//		btnEquals.setPrefWidth(70);
+		txtNum1.setPrefWidth(70);
+		txtNum2.setPrefWidth(70);
+		btnClear.setPrefWidth(150);
+		ansLabel.setPrefWidth(150);
+	}
+	
+	
+	public static void main(String[] args) {
         launch(args);
     }
 }
